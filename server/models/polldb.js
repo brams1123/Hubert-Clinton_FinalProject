4900e73c8de8a8122094225402aee576f0aac31c
brams1123 <bramsosifo@gmail.com>
2017-04-15 14:27:27 -0400

    creating a poll answer option

--- server/models/polldb.js
+++ server/models/polldb.js
@@ -3,6 +3,7 @@ let mongoose = require('mongoose');
 // create a model class
 let contactSchema = mongoose.Schema({
     question: String,
+    option: String,
     option1: String,
     option2: String,
     option3: String
